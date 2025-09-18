aed501657b3272c8bb559d7b667252353aafe334
Fragox77 <62865892+Fragox77@users.noreply.github.com>
2025-09-17 23:49:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Esta es la ruta base para el despliegue en GitHub Pages.
+      // Debe coincidir con el nombre de tu repositorio.
+      base: '/Prototipo-Portafolio/',
+      
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
